966b92fb103019537b5e835fbdc26073859f7fbd
jwheller0013 <jwheller0013@yahoo.com>
2025-03-08 08:15:42 -0500

    Working on PasswordValidatorTest

--- TDDOne/tddone/src/test/java/rocks/zipcode/PasswordValidatorTest.java
+++ TDDOne/tddone/src/test/java/rocks/zipcode/PasswordValidatorTest.java
@@ -1,4 +1,5 @@
 package rocks.zipcode;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,6 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 // Student should write this test class
 public class PasswordValidatorTest {
     // TODO: Write tests for the PasswordValidator class
+    private PasswordValidator passwordValidator= new PasswordValidator();
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+//    @Test
+//    void testIsValid () {
+//        this.passwordValidator = PasswordValidator;
+//        String passwordnull = "bob";
+//        assertEquals(true, passwordValidator.isValid(passwordnull));
+//    }
+
+    @Test
+    void testContainsUppercase () {
+        String passwordlower = "lowercase";
+        assertEquals(false, );
+    }
+
+
     // Test cases should include:
     // - Valid passwords
     // - Passwords that are too short or too long
